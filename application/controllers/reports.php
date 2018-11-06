57e1fa706178f37c62d5ad20bb93850a53836fd0
Felix0103 <datahernandez@gmail.com>
2018-11-06 00:04:24 -0400

    update casa

---
+++ application/controllers/reports.php
@@ -0,0 +1,14 @@
+ <?php
+ini_set('memory_limit', '-1');
+ini_set('display_errors', '1');
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class reports extends Front_Controller
+{	
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+}
